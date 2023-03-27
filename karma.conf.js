53f0f679eacc0e870bb4b9b24c7004919cf0d1e7
RashmieS <rashmi02deepti@gmail.com>
2023-03-27 15:51:05 +0530

    modify repo name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/notes-manager'),
+      dir: require('path').join(__dirname, './coverage/notes-manager-dashboard'),
       subdir: '.',
       reporters: [
         { type: 'html' },
